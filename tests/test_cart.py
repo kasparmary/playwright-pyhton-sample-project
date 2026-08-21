afe2b153081940dd695604495652c6d423cd6683
kaspar-mary-ZS0390 <kaspar.mary@zucisystems.com>
2026-08-21 11:03:07 +0530

    created env and did installation againa

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -5,9 +5,7 @@ from playwright.sync_api import Page
 
 def test_navigate_to_cart(page:Page, home_page, cart_page) -> None:
     home_page.open()
-    # login_page = home_page.go_to_login()
     cart_page.navigate_to_cart()
     cart_page.click_cart_items_link()
-    # cart_page.click_product_overlay()
     cart_page.click_add_to_cart()
     cart_page.click_view_cart_link()
